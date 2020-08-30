901bee3381ff33604ee36da003d3e82593f83189
Javier Soto <ios@javisoto.es>
2020-08-29 17:13:31 -0700

    Remove assertions

--- Sources/ColonyPigmentationAnalysisKit/Colors.swift
+++ Sources/ColonyPigmentationAnalysisKit/Colors.swift
@@ -227,8 +227,6 @@ public struct LABColor: Hashable {
             + pow(colorNormalizedComponents.b - selfNormalizedComponents.b, 2)
         )
         
-        ColonyPigmentationAnalysisKit.assert(distance.isNormalized)
-        
         return distance
     }
     
@@ -243,7 +241,6 @@ public struct LABColor: Hashable {
         let distanceToCenterOfSphere = distance(to: colorAtCenterOfSphere, ignoringLightness: ignoringLightness)
         let sphereRadius: Double = 0.5
         let maximumDistance = distanceToCenterOfSphere + sphereRadius
-        ColonyPigmentationAnalysisKit.assert((0...1.1).contains(maximumDistance))
         
         let distance = self.distance(to: color, ignoringLightness: ignoringLightness)
         
